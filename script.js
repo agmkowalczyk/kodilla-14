06b34ff00ef6c6f149fb6fe915ca0d65b1fc21e1
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-11-14 12:05:00 +0100

    ReactJS - Movies

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var movies = [
+var arrMovies = [
   {
     id: 1,
     title: 'Harry Potter',
@@ -19,18 +19,106 @@ var movies = [
   }
 ];
 
-var moviesElements = movies.map(function(movie) {
-  return React.createElement('li', {key: movie.id},
-      React.createElement('h2', {}, movie.title),
-      React.createElement('p', {}, movie.desc),
-      React.createElement('img', {src: movie.img})
-    );
+var Movies = React.createClass({  
+  render: function() { 
+  return (
+      React.createElement('div', {},
+      React.createElement('h1', {}, 'Lista filmów'),
+      React.createElement(MoviesList, {movies: arrMovies})
+      )
+    )
+  }
 });
 
-var element =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'Lista filmów'),
-    React.createElement('ul', {}, moviesElements)
-  );
+var MoviesList = React.createClass({
+  propTypes: {
+    movies: React.PropTypes.array.isRequired,
+  },  
+  render: function() { 
+  var moviesElements = this.props.movies.map(function(allMovies) { 
+  return (
+      React.createElement(MovieItem, {key:allMovies.id, movie:allMovies})
+    )
+  });
+  return (
+      React.createElement('ul', {}, moviesElements)
+    );  
+  } 
 
+});
+
+var MovieItem = React.createClass({
+  propTypes: {
+    movie: React.PropTypes.object.isRequired,
+  },  
+  render: function() {
+  return (
+      React.createElement('li', {},
+        React.createElement('h2', {}, this.props.movie.title),      
+        React.createElement(MovieDescription, {movieDesc: this.props.movie.desc}),
+        React.createElement(MovieImage, {movieImg: this.props.movie.img})
+      )
+    );
+  }
+});
+
+var MovieDescription = React.createClass({
+  propTypes: {
+    movieDesc: React.PropTypes.string.isRequired,
+  },
+  render: function() {
+  return (
+      React.createElement('p', {}, 'opis: ' + this.props.movieDesc)
+    )
+  }
+});
+
+var MovieImage = React.createClass({
+  propTypes: {
+    movieImg: React.PropTypes.string.isRequired,
+  },
+  render: function() {
+  return (
+      React.createElement('img', {src: this.props.movieImg})
+    )
+  }
+});
+
+var movies = React.createElement(Movies);
+ReactDOM.render(movies, document.getElementById('app'));
+
+
+
+
+
+
+
+
+
+/*
+// KOTEK
+var GalleryItem = React.createClass({
+  propTypes: {
+    image: React.PropTypes.object.isRequired,
+  },
+
+    render: function() {
+    return (
+        React.createElement('div', {},
+        React.createElement('h2', {}, this.props.image.name),
+        React.createElement('p', {}, this.props.image.desc),
+        React.createElement('img', {src: this.props.image.src})
+      )
+    )
+  },
+});
+
+var image111 = {  
+  name: 'Kotek',
+  desc: 'mały kotek',
+  src: 'http://imgur.com/n8OYCzR.png'
+};
+
+var element = React.createElement(GalleryItem, {image: image111});
 ReactDOM.render(element, document.getElementById('app'));
+*/
